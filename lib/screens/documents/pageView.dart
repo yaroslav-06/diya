199880c4905b5dac65619f13dcc98cbd77a95182
Yaroslav <yparashchiy.2006@gmal.com>
2021-07-12 16:03:50 +0300

    xz

--- lib/screens/documents/pageView.dart
+++ lib/screens/documents/pageView.dart
@@ -5,13 +5,12 @@ import 'package:flutter/material.dart';
 
 import 'notInternet.dart';
 
-Color lighter(Color a, double k){
+Color lighter(Color a, double k) {
   return Color.fromRGBO(
-    min(255, (a.red+(255-a.red)*k).round()),
-    min(255, (a.green+(255-a.green)*k).round()),
-    min(255, (a.blue+(255-a.blue)*k).round()),
-    1
-  );
+      min(255, (a.red + (255 - a.red) * k).round()),
+      min(255, (a.green + (255 - a.green) * k).round()),
+      min(255, (a.blue + (255 - a.blue) * k).round()),
+      1);
 }
 
 class CPageView extends StatefulWidget {
@@ -26,44 +25,44 @@ PageController? pwContr;
 class _CPageViewState extends State<CPageView> {
   @override
   void initState() {
-    pwContr=PageController(
+    pwContr = PageController(
       initialPage: 0,
       viewportFraction: 0.85,
     );
     super.initState();
   }
+
   @override
-  void dispose() { 
+  void dispose() {
     pwContr!.dispose();
     super.dispose();
   }
-  addLisPage(){
+
+  addLisPage() {
     curColorInt = pwContr!.page!.floor();
     curColor.value = pwContr!.page!.round();
-    changing.value = pwContr!.page!-curColorInt;
-    scales[curColorInt].value=1-changing.value*0.1;
-    if(curColorInt!=changinColor.length-2)
-      scales[curColorInt+1].value=0.9+changing.value*0.1;
+    changing.value = pwContr!.page! - curColorInt;
+    scales[curColorInt].value = 1 - changing.value * 0.1;
+    if (curColorInt != changinColor.length - 2)
+      scales[curColorInt + 1].value = 0.9 + changing.value * 0.1;
   }
+
   @override
   Widget build(BuildContext context) {
-    changing.value=0;
+    changing.value = 0;
     pwContr!.addListener(addLisPage);
     return PageView(
       physics: BouncingScrollPhysics(),
       controller: pwContr,
-      children: List.generate(changinColor.length-1, (i){
+      children: List.generate(changinColor.length - 1, (i) {
         return Center(
           child: CMatrixTransition(
-            back: PassCard(
-              child: NotInternet(ind: i,),
-              scaleCh: scales[i]
-            ),
-            front: PassCard(
-              child: widget.children![i],
-              scaleCh: scales[i]
-            )
-          ),
+              back: PassCard(
+                  child: NotInternet(
+                    ind: i,
+                  ),
+                  scaleCh: scales[i]),
+              front: PassCard(child: widget.children![i], scaleCh: scales[i])),
         );
       }),
     );
@@ -73,56 +72,59 @@ class _CPageViewState extends State<CPageView> {
 class CMatrixTransition extends StatefulWidget {
   final Widget? front, back;
   final Duration dur;
-  CMatrixTransition({@required this.front,@required this.back, this.dur=const Duration(milliseconds: 16*600)});
+  CMatrixTransition(
+      {@required this.front,
+      @required this.back,
+      this.dur = const Duration(milliseconds: 16 * 600)});
   @override
   _MatrixTransitionState createState() => _MatrixTransitionState();
 }
 
-class _MatrixTransitionState extends State<CMatrixTransition> with SingleTickerProviderStateMixin {
+class _MatrixTransitionState extends State<CMatrixTransition>
+    with SingleTickerProviderStateMixin {
   AnimationController? anC;
-  bool side=true;
+  bool side = true;
   @override
   void initState() {
-    anC=AnimationController(
-      vsync: this,
-      duration: widget.dur,
-      reverseDuration: widget.dur,
-      upperBound: 16
-    );
+    anC = AnimationController(
+        vsync: this,
+        duration: widget.dur,
+        reverseDuration: widget.dur,
+        upperBound: 16);
     super.initState();
   }
+
   @override
   void dispose() {
     anC!.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
-        anC!.animateTo(anC!.value+1);
+      onTap: () {
+        anC!.animateTo(anC!.value + 1);
       },
       child: AnimatedBuilder(
-        animation: anC!,
-        builder: (context, child){
-          side=anC!.value.round()%2==0;
-          return Center(
-            child: Transform(
-              transform: Matrix4.identity()
-                ..setEntry(2, 3, 0.001)
-                ..rotateY(anC!.value*pi),
-              alignment: Alignment.center,
-              child: side
-                ?widget.front
-                :Transform(
-                  alignment: Alignment.center,
-                  transform: Matrix4.identity()..rotateY(pi),
-                  child: widget.back
-                ),
-            ),
-          );
-        }
-      ),
+          animation: anC!,
+          builder: (context, child) {
+            side = anC!.value.round() % 2 == 0;
+            return Center(
+              child: Transform(
+                transform: Matrix4.identity()
+                  ..setEntry(2, 3, 0.001)
+                  ..rotateY(anC!.value * pi),
+                alignment: Alignment.center,
+                child: side
+                    ? widget.front
+                    : Transform(
+                        alignment: Alignment.center,
+                        transform: Matrix4.identity()..rotateY(pi),
+                        child: widget.back),
+              ),
+            );
+          }),
     );
   }
 }
@@ -137,34 +139,32 @@ class PassCard extends StatelessWidget {
     return ValueListenableBuilder<double>(
       valueListenable: scaleCh!,
       builder: (context, x, child) {
-        return Transform.scale(
-          scale: x,
-          child: child
-        );
+        return Transform.scale(scale: x, child: child);
       },
       child: ValueListenableBuilder<double>(
-        valueListenable: changing,
-        child: child,
-        builder: (context, ans, child) {
-          return Container(
-            height: width*0.3*3.9,
-            width: width*0.95,
-            decoration: BoxDecoration(
-              color: lighter(curDocColor, 0.5),
-              borderRadius: BorderRadius.circular(8),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.4),
-                  spreadRadius: -10,
-                  blurRadius: 7,
-                  offset: Offset(0, 25),
-                ),
-              ],
-            ),
-            child: child
-          );
-        }
-      ),
+          valueListenable: changing,
+          child: child,
+          builder: (context, ans, child) {
+            return AspectRatio(
+              aspectRatio: 3 / 4,
+              child: Container(
+                  //height: width*0.3*3.6,
+                  width: width * 0.95,
+                  decoration: BoxDecoration(
+                    color: lighter(curDocColor, 0.5),
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.4),
+                        spreadRadius: -10,
+                        blurRadius: 7,
+                        offset: Offset(0, 25),
+                      ),
+                    ],
+                  ),
+                  child: child),
+            );
+          }),
     );
   }
 }
--- lib/screens/documents/passport.dart
+++ lib/screens/documents/passport.dart
@@ -6,84 +6,80 @@ class Passport extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: width*0.95,
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: width/22.5),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              width: width*0.95,
-              padding: EdgeInsets.symmetric(vertical: width/13.3),
-              child: Text(
-                "Паспорт громадянина\nУкраїни <тризуб>",
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w500
+        width: width * 0.95,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: width / 22.5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: width * 0.95,
+                padding: EdgeInsets.symmetric(vertical: width / 13.3),
+                child: Text(
+                  "Паспорт громадянина\nУкраїни <тризуб>",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
                 ),
               ),
-            ),
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: FittedBox(
-                      child: Container(
-                        child: Image(image: NetworkImage(photoURL!)),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            width: width/180,
-                            color: passportColor
-                          )
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Center(
+                        child: AspectRatio(
+                          aspectRatio: 3 / 4,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: NetworkImage(photoURL!),
+                                    fit: BoxFit.cover),
+                                border:
+                                    Border.all(width: 2, color: passportColor)),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      //height: (width*0.95/2-39)/3*4,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.symmetric(vertical: width/180, horizontal: width/22.5),
-                            child: Text('Дата\nнародження:\n'+birthDate!),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(width/22.5),
-                            child: Text('Номер:\n005482371'),
-                          ),
-                        ],
+                    Expanded(
+                      child: Container(
+                        //height: (width*0.95/2-39)/3*4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: width / 180,
+                                  horizontal: width / 22.5),
+                              child: Text('Дата\nнародження:\n' + birthDate!),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(width / 22.5),
+                              child: Text('Номер:\n005482371'),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              height: 2,
-              width: width*0.95,
-              margin: EdgeInsets.only(top: width*29/360),
-              color: passportColor
-            ),
-            Container(
-              height: width*0.95/3,
-              padding: EdgeInsets.only(top: width/30),
-              alignment: Alignment.topLeft,
-              child: Text(
-                name!,
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w500
+                    )
+                  ],
                 ),
               ),
-            )
-          ],
-        ),
-      )
-    );
+              Container(
+                  height: 2,
+                  width: width * 0.95,
+                  margin: EdgeInsets.only(top: width * 29 / 360),
+                  color: passportColor),
+              Container(
+                height: width * 0.95 / 3,
+                padding: EdgeInsets.only(top: width / 30),
+                alignment: Alignment.topLeft,
+                child: Text(
+                  name!,
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
